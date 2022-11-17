b354cb167904b0fe2676b1f094d533c5c7f6a7cd
tatsiana <Tatsiana3345260@gmail.com>
2022-11-17 11:10:04 +0300

    add page

--- styles/tanya.js
+++
@@ -1,6 +0,0 @@
-$('#timer').countdown('2023/01/01').on('update.countdown', function(event) {
-    var $this = $(this).html(event.strftime(''
-      + '<span>%-w</span> недель'
-      + '<span>%-d</span> дней'
-      ));
-  });
